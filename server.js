acaccd8a98ae93da539a11399914fd277e784222
kaanturer <220803062@ogrenci.artvin.edu.tr>
2026-08-02 03:33:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public'));
+app.use(express.static('public', { index: false }));
 
 const activeRooms = new Map();
 
